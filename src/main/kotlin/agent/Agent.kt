e18e4ad0a55576fa8c151073083559d00582c2d2
Kirill <timofeev.kirill.a@gmail.com>
2017-05-14 21:17:12 +0300

    done

--- src/main/kotlin/agent/Agent.kt
+++ src/main/kotlin/agent/Agent.kt
@@ -1,12 +1,61 @@
 package agent
 
+import org.objectweb.asm.*
+import org.objectweb.asm.ClassWriter.COMPUTE_MAXS
+import java.lang.instrument.ClassFileTransformer
 import java.lang.instrument.Instrumentation
+import java.security.ProtectionDomain
 
 class Agent {
     companion object {
+        // the idea is to make this code as simple is possible.
+        // no need to use asm.commons.*
         @JvmStatic
         fun premain(agentArgs: String?, inst: Instrumentation) {
             println("Agent started.")
+            inst.addTransformer(TestTransformer())
         }
+    }
+}
+
+class TestTransformer : ClassFileTransformer {
+    override fun transform(loader: ClassLoader?, className: String?, classBeingRedefined: Class<*>?,
+                           protectionDomain: ProtectionDomain?, classfileBuffer: ByteArray?): ByteArray {
+        val reader = ClassReader(classfileBuffer)
+        val writer = ClassWriter(reader, COMPUTE_MAXS)
+        val visitor = MyClassVisitor(writer)
+        reader.accept(visitor, 0)
+        return writer.toByteArray()
+    }
+}
+
+class MyClassVisitor(cv: ClassVisitor) : ClassVisitor(Opcodes.ASM5, cv) {
+    override fun visitMethod(access: Int, name: String?, desc: String?, signature: String?,
+                             exceptions: Array<out String>?): MethodVisitor? {
+        cv ?: return null // just in case
+        return MyMethodVisitor(cv.visitMethod(access, name, desc, signature, exceptions))
+    }
+}
+
+class MyMethodVisitor(mv: MethodVisitor) : MethodVisitor(Opcodes.ASM5, mv) {
+    companion object {
+        private val METHOD_OPCODE = Opcodes.INVOKESTATIC
+        private val METHOD_OWNER = "example/CoroutineExampleKt"
+        private val METHOD_NAME = "test"
+        private val METHOD_SIGNATURE = "(Lkotlin/coroutines/experimental/Continuation;)Ljava/lang/Object;"
+
+        private data class MyInstruction(val owner: String, val name: String, val desc: String)
+
+        private val systemOut = MyInstruction("java/lang/System", "out", "Ljava/io/PrintStream;")
+        private val outPrintln = MyInstruction("java/io/PrintStream", "println", "(Ljava/lang/String;)V")
+    }
+
+    override fun visitMethodInsn(opcode: Int, owner: String?, name: String?, desc: String?, itf: Boolean) {
+        if (opcode == METHOD_OPCODE && owner == METHOD_OWNER && name == METHOD_NAME && desc == METHOD_SIGNATURE) {
+            mv.visitFieldInsn(Opcodes.GETSTATIC, systemOut.owner, systemOut.name, systemOut.desc)
+            mv.visitLdcInsn("Test detected")
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, outPrintln.owner, outPrintln.name, outPrintln.desc, false)
+        }
+        mv.visitMethodInsn(opcode, owner, name, desc, itf)
     }
 }
--- src/test/kotlin/example/CoroutineExample.kt
+++ src/test/kotlin/example/CoroutineExample.kt
@@ -6,9 +6,21 @@ import kotlinx.coroutines.experimental.runBlocking
 fun main(args: Array<String>) = runBlocking<Unit> {
     println("Started!")
     test()
+    test(1234)
+    testInside()
     println("Done.")
 }
 
+suspend fun testInside() {
+    test()
+}
+
+suspend fun test(time: Long) {
+    println("test($time)")
+    delay(time)
+}
+
 suspend fun test() {
+    println("test()")
     delay(1000)
 }
